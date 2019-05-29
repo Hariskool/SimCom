a023cabf95b9f1f99276ae2a14fc5c4ca6ced713
simcoeit <simcoeit@simcoeits-Mac.local>
2019-05-29 16:54:47 +0500

    Initial Commit

---
+++ SimCom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimCom
+//
+//  Created by simcoeit on 5/29/19.
+//  Copyright © 2019 simcoeit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
